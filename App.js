8f9f1f4fe89961236fe8120e7bf2cc71eb5e133a
ahsanaah29-hub <ahsanaah29@gmail.com>
2025-08-12 11:02:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+// PASTE THE COMPLETE BEAUTIFUL INTERFACE CODE HERE
+// (The code from the artifact I created above - the one with the blue theme)
+
+import React, { useState } from 'react'
+import { Search, BookOpen, FileText, Video, Image, ChevronDown, ChevronUp, HelpCircle, ExternalLink } from 'lucide-react'
+
+// ... rest of the component code goes here
